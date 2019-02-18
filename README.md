c0aed6d8d48782745237259d479d74597f1700cd
Myk Willis <myk@mykwillis.com>
2019-02-18 15:03:28 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Direqt - Facebook Messenger Integration
+
+This example application demonstrates how the Direqt platform can be integrated into a Facebook Messenger bot. It implements a Facebook *webhook*, which is the mechanism by which the Messenger Platform notifies a bot of various interactions and events.
+
+For more information on Facebook's Messenger Platform, including how to configure and deploy a webhook, see: https://developers.facebook.com/docs/messenger-platform/webhook
+
+### Setup
+
+Facebook Messenger bots are associated with specific Facebook Pages. This
+association is performed with a Facebook app, which must be configured to
+refer to your webhook. The initial setup of these FB entities is a bit involved, but well documented here:
+
+https://developers.facebook.com/docs/messenger-platform/getting-started/app-setup
+
+Once you have followed the instructions from the link above to create a Facebook App (and Page, if necessary), you are ready to run this example.
+
+While you could upload the example to a web host for testing, you'll probably find it easiest to simply run it on your local development machine, and use `ngrok` to expose it to the internet.
+
+Download and install `ngrok` here: https://ngrok.com/download
+
+Then you can simply:
+
+```
+    $ node index.js     # launch webhook server on port 1337 (default)
+    $ ngrok http 1337   # expose port 1337 to the web
+```
+   
+Note the https address listed for "Forwarding" in the ngrok output. It will look like "https://<unique>.ngrok.io". This is the "Forwarding URL" that you'll use below. 
+
+1. Navigate to https://developers.facebook.com/apps/
+2. Select the Facebook App hosting your webhook
+3. Tap "Webhooks" from the left menu column
+4. Tap "Edit Subscription"
+5. Enter the "Forwarding url" from `ngrok` into the box labeled "Callback URL"
+6. Enter your VERIFY_TOKEN into the box labeled "Verify Token"
+7. Tap "Verify and Save"
+
+
+### Running the Example
+
+Once installed, you visit the Facebook Page associated with the Messenger bot, and tap the "Message" button. Type a message into the Messenger interface, and the bot should respond with a simple echo.
+
+The example has some keywords ("hello", "goodbye") that are mapped to Direqt moments, so by typing these in your message, you will receive content served from Direqt.
+  
+   
+### Using an alternate Direqt account
+
+This example is shipped with a Direqt API key that refers to a "playground" account that has been preconfigured with components and other information necessary to test interactions. If you'd rather use your own Direqt account, change the DIREQT_API_KEY constant in `index.js` to refer to your own API key, which can be obtained from the "API Keys" option in the Direqt Console (<https://console.direqt.io>). 
+
+
+### Documentation
+
+See <https://docs.direqt.io> for full Direqt documentation.
+
+Copyright (c) 2017-2019 Direqt Inc. All Rights Reserved.
+
---
+++ index.js
@@ -0,0 +1,164 @@
+'use strict';
+
+/**
+ * Facebook Messenger Webhook with Direqt Platform Integration.
+ *
+ * Available environment variable overrides (see detailed descriptions at
+ * definition below):
+ *
+ *  PAGE_ACCESS_TOKEN - obtained from Facebook Messenger settings.
+ *  VERIFY_TOKEN - provided by webook (not necessary for testing).
+ *  DIREQT_API_KEY - obtained from Direqt Console (not necessary for testing).
+ */
+const express = require('express');
+const request = require('request');
+const app = express().use(require('body-parser').json());
+
+/**
+ * Page Access token supplied by Facebook.
+ *
+ * Obtain this token for the appropriate page by visiting:
+ *  Messenger settings console | "Token Generation" | "Select a Page"
+ */
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN || "<invalid>";
+
+/**
+ * Verify token for our webhook.
+ *
+ * This token is defined by the webhook, and provided to FB via:
+ *  Messenger settings console | "Setup Webhooks" | "Verify Token"
+ *
+ * You should change this token if you are deploying a production webhook.
+ */
+let VERIFY_TOKEN = process.env.VERIFY_TOKEN || "<provide-your-own-secure-token>";
+
+/**
+ * Direqt API key.
+ *
+ * You can leave this unchanged to use the playground API key, or you can
+ * supply your own API key which can be obtained from the Direqt Console
+ * (https://console.direqt.io) at:
+ *
+ *  Direqt Console | <Account Name> | "API Keys"
+ */
+const DIREQT_PLAYGROUND_API_KEY = "5rp26o1WB5IBQ6gVTg"; // acceptable for use in testing
+const DIREQT_API_KEY = process.env.DIREQT_API_KEY || DIREQT_PLAYGROUND_API_KEY
+
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+/**
+ * Endpoint invoked by FB Messenger to verify our webhook.
+ */
+app.get('/webhook', (req, res) => {
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+/**
+ * Endpoint invoked by FB to notify us of messaging events.
+ */
+app.post('/webhook', (req, res) => {
+    let body = req.body;
+
+    if (body.object === 'page') {
+        body.entry.forEach(function(entry) {
+            let webhook_event = entry.messaging[0];
+            let sender_psid = webhook_event.sender.id;
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            }
+        });
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+/**
+ * Process a (text) message received by the sender (ie, end user).
+ *
+ * For this example, we normally just do a simple echo back to the sender.
+ * To demonstrate interactions with Direqt, though, we look for a few keywords,
+ * which are mapped to "moments" that have been predefined in the Direqt
+ * playground account.
+ *
+ * In your own bot, you can define your own moments, and trigger them according
+ * to your own application logic. Refer to https://docs.direqt.io for more
+ * info.
+ */
+function handleMessage(sender_psid, received_message) {
+    let response;
+    if (!received_message.text) {
+        return
+    }
+
+    response = {
+        "text": `You said: "${received_message.text}"`
+    };
+    callSendAPI(sender_psid, response);
+
+    if (received_message.text.includes("offer")) {
+        fetchDireqt(sender_psid, "moment-offer");
+    } else if (received_message.text.includes("goodbye")) {
+        fetchDireqt(sender_psid, "moment-goodbye");
+    }
+}
+
+function callSendAPI(sender_psid, response) {
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    };
+
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+        } else {
+            console.error("Facebook send request failed:" + err)
+        }
+    });
+}
+
+function fetchDireqt(sender_psid, moment) {
+    let request_body = {
+        format: "FBM",
+        "moment": moment,
+        "subscriber": sender_psid,
+        // You may pass any non-personally-identifiable information you have
+        // about the subscriber in the targeting object.
+        "targeting": `{
+            "language": "en"
+        }`
+    };
+
+    request({
+        "uri": "https://api.direqt.io/fetch",
+        "qs": { "key": DIREQT_API_KEY },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            if (body && body.payload) {
+                callSendAPI(sender_psid, JSON.parse(body.payload));
+            } else {
+                console.log("Direqt fetch was empty.")
+            }
+        } else {
+            console.error("Direqt fetch request failed:" + err)
+        }
+    });
+}
